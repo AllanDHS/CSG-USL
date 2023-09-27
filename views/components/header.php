79745aaa8832047db89673dcf9b373d821c30574
AllanDHS <allandehors@icloud.com>
2023-09-27 16:54:57 +0200

    debutProjet

---
+++ views/components/header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../assets/style/style.css">
+    
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+    <title>CSG-USL HANDBALL</title>
+</head>
+<body>
