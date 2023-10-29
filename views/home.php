db9cc7806f0728b4da72944330ac8e9cfdb9347a
Allan DEHORS <allandhs@MacBook-Pro-de-Allan.local>
2023-10-29 22:30:01 +0100

    Article/ResponsiveHomePage

--- views/home.php
+++ views/home.php
@@ -17,87 +17,7 @@
 <div class="matchHomePage">
     <div class="title">
         <a href="">
-            <p>Match a venir</p>
-        </a>
-    </div>
-<div class="container-fluid">
-        <div class="row mx-auto">
-            <div class="col-md-2 col-sm-5 col-5 red">
-                <div class="top">
-                    <p>Séniors 1M</p>
-                    <p>29/10/2023</p>
-                    <p>21H</p>
-                    <p>Salle Michel Ostermeyer</p>
-                    <p>Lillebonne</p>
-                </div>
-                <div class="center">
-                    <img src="Logo_CSG-1920w-removebg-preview.png" alt="Logo Equipe 1" class="Logo1">
-                    <p class="mx-auto">VS</p>
-                    <img src="Logo_CSG-1920w-removebg-preview.png" alt="Logo Equipe 2" class="Logo2">
-                </div>
-                <div class="bottom">
-                    <p>pré-national</p>
-                    <p></p>
-                </div>
-            </div>
-            <div class="col-md-2 col-sm-5 col-5 red">
-                <div class="top">
-                    <p>Séniors 1M</p>
-                    <p>29/10/2023</p>
-                    <p>21H</p>
-                    <p>Salle Michel Ostermeyer</p>
-                    <p>Lillebonne</p>
-                </div>
-                <div class="center">
-                    <img src="Logo_CSG-1920w-removebg-preview.png" alt="Logo Equipe 1" class="Logo1">
-                    <p class="mx-auto">VS</p>
-                    <img src="Logo_CSG-1920w-removebg-preview.png" alt="Logo Equipe 2" class="Logo2">
-                </div>
-                <div class="bottom">
-                    <p>pré-national</p>
-                </div>
-            </div>
-            <div class="col-md-2 col-sm-5 col-5 red">
-                <div class="top">
-                    <p>Séniors 1M</p>
-                    <p>29/10/2023</p>
-                    <p>21H</p>
-                    <p>Salle Michel Ostermeyer</p>
-                    <p>Lillebonne</p>
-                </div>
-                <div class="center">
-                    <img src="Logo_CSG-1920w-removebg-preview.png" alt="Logo Equipe 1" class="Logo1">
-                    <p class="mx-auto">VS</p>
-                    <img src="Logo_CSG-1920w-removebg-preview.png" alt="Logo Equipe 2" class="Logo2">
-                </div>
-                <div class="bottom">
-                    <p>pré-national</p>
-                </div>
-            </div>
-            <div class="col-md-2 col-sm-5 col-5 red">
-                <div class="top">
-                    <p>Séniors 1M</p>
-                    <p>29/10/2023</p>
-                    <p>21H</p>
-                    <p>Salle Michel Ostermeyer</p>
-                    <p>Lillebonne</p>
-                </div>
-                <div class="center">
-                    <img src="Logo_CSG-1920w-removebg-preview.png" alt="Logo Equipe 1" class="Logo1">
-                    <p class="mx-auto">VS</p>
-                    <img src="Logo_CSG-1920w-removebg-preview.png" alt="Logo Equipe 2" class="Logo2">
-                </div>
-                <div class="bottom">
-                    <p>pré-national</p>
-                </div>
-            </div>
-        </div>
-    </div>
-
-<div class="matchHomePage">
-    <div class="title">
-        <a href="">
-            <p>Match a venir</p>
+            <p>resultat du week-end</p>
         </a>
     </div>
     <div class="container-fluid">
@@ -111,9 +31,11 @@
                     <p>Lillebonne</p>
                 </div>
                 <div class="center">
-                    <img src="Logo_CSG-1920w-removebg-preview.png" alt="Logo Equipe 1" class="Logo1">
-                    <p class="mx-auto">VS</p>
-                    <img src="Logo_CSG-1920w-removebg-preview.png" alt="Logo Equipe 2" class="Logo2">
+                    <img src="../assets/logo/Logo_CSG-1920w-removebg-preview.png" alt="Logo Equipe 1" class="Logo1">
+                    <p class="score">30</p>
+                    <p class="mx-auto">-</p>
+                    <p class="score">20</p>
+                    <img src="../assets/logo/Logo_CSG-1920w-removebg-preview.png" alt="Logo Equipe 2" class="Logo2">
                 </div>
                 <div class="bottom">
                     <p>pré-national</p>
@@ -129,9 +51,11 @@
                     <p>Lillebonne</p>
                 </div>
                 <div class="center">
-                    <img src="Logo_CSG-1920w-removebg-preview.png" alt="Logo Equipe 1" class="Logo1">
-                    <p class="mx-auto">VS</p>
-                    <img src="Logo_CSG-1920w-removebg-preview.png" alt="Logo Equipe 2" class="Logo2">
+                    <img src="../assets/logo/Logo_CSG-1920w-removebg-preview.png" alt="Logo Equipe 1" class="Logo1">
+                    <p class="score">30</p>
+                    <p class="mx-auto">-</p>
+                    <p class="score">20</p>
+                    <img src="../assets/logo/Logo_CSG-1920w-removebg-preview.png" alt="Logo Equipe 2" class="Logo2">
                 </div>
                 <div class="bottom">
                     <p>pré-national</p>
@@ -146,9 +70,11 @@
                     <p>Lillebonne</p>
                 </div>
                 <div class="center">
-                    <img src="Logo_CSG-1920w-removebg-preview.png" alt="Logo Equipe 1" class="Logo1">
-                    <p class="mx-auto">VS</p>
-                    <img src="Logo_CSG-1920w-removebg-preview.png" alt="Logo Equipe 2" class="Logo2">
+                    <img src="../assets/logo/Logo_CSG-1920w-removebg-preview.png" alt="Logo Equipe 1" class="Logo1">
+                    <p class="score">30</p>
+                    <p class="mx-auto">-</p>
+                    <p class="score">20</p>
+                    <img src="../assets/logo/Logo_CSG-1920w-removebg-preview.png" alt="Logo Equipe 2" class="Logo2">
                 </div>
                 <div class="bottom">
                     <p>pré-national</p>
@@ -163,9 +89,11 @@
                     <p>Lillebonne</p>
                 </div>
                 <div class="center">
-                    <img src="Logo_CSG-1920w-removebg-preview.png" alt="Logo Equipe 1" class="Logo1">
-                    <p class="mx-auto">VS</p>
-                    <img src="Logo_CSG-1920w-removebg-preview.png" alt="Logo Equipe 2" class="Logo2">
+                    <img src="../assets/logo/Logo_CSG-1920w-removebg-preview.png" alt="Logo Equipe 1" class="Logo1">
+                    <p class="score">30</p>
+                    <p class="mx-auto">-</p>
+                    <p class="score">20</p>
+                    <img src="../assets/logo/Logo_CSG-1920w-removebg-preview.png" alt="Logo Equipe 2" class="Logo2">
                 </div>
                 <div class="bottom">
                     <p>pré-national</p>
@@ -174,28 +102,124 @@
         </div>
     </div>
 
-</div>
-<div class="actuHomePage">
-    <div class="title">
-        <a href="">
-            <p>nos dernière <br>Actualités</p>
-        </a>
+    <div class="matchHomePage">
+        <div class="title">
+            <a href="">
+                <p>Match a venir</p>
+            </a>
+        </div>
+        <div class="container-fluid">
+            <div class="row mx-auto">
+                <div class="col-md-2 col-sm-5 col-5 red">
+                    <div class="top">
+                        <p>Séniors 1M</p>
+                        <p>29/10/2023</p>
+                        <p>21H</p>
+                        <p>Salle Michel Ostermeyer</p>
+                        <p>Lillebonne</p>
+                    </div>
+                    <div class="center">
+                        <img src="../assets/logo/Logo_CSG-1920w-removebg-preview.png" alt="Logo Equipe 1" class="Logo1">
+                        <p class="mx-auto">VS</p>
+                        <img src="../assets/logo/Logo_CSG-1920w-removebg-preview.png" alt="Logo Equipe 2" class="Logo2">
+                    </div>
+                    <div class="bottom">
+                        <p>pré-national</p>
+                        <p></p>
+                    </div>
+                </div>
+                <div class="col-md-2 col-sm-5 col-5 red">
+                    <div class="top">
+                        <p>Séniors 1M</p>
+                        <p>29/10/2023</p>
+                        <p>21H</p>
+                        <p>Salle Michel Ostermeyer</p>
+                        <p>Lillebonne</p>
+                    </div>
+                    <div class="center">
+                        <img src="../assets/logo/Logo_CSG-1920w-removebg-preview.png" alt="Logo Equipe 1" class="Logo1">
+                        <p class="mx-auto">VS</p>
+                        <img src="../assets/logo/Logo_CSG-1920w-removebg-preview.png" alt="Logo Equipe 2" class="Logo2">
+                    </div>
+                    <div class="bottom">
+                        <p>pré-national</p>
+                    </div>
+                </div>
+                <div class="col-md-2 col-sm-5 col-5 red">
+                    <div class="top">
+                        <p>Séniors 1M</p>
+                        <p>29/10/2023</p>
+                        <p>21H</p>
+                        <p>Salle Michel Ostermeyer</p>
+                        <p>Lillebonne</p>
+                    </div>
+                    <div class="center">
+                        <img src="../assets/logo/Logo_CSG-1920w-removebg-preview.png" alt="Logo Equipe 1" class="Logo1">
+                        <p class="mx-auto">VS</p>
+                        <img src="../assets/logo/Logo_CSG-1920w-removebg-preview.png" alt="Logo Equipe 2" class="Logo2">
+                    </div>
+                    <div class="bottom">
+                        <p>pré-national</p>
+                    </div>
+                </div>
+                <div class="col-md-2 col-sm-5 col-5 red">
+                    <div class="top">
+                        <p>Séniors 1M</p>
+                        <p>29/10/2023</p>
+                        <p>21H</p>
+                        <p>Salle Michel Ostermeyer</p>
+                        <p>Lillebonne</p>
+                    </div>
+                    <div class="center">
+                        <img src="../assets/logo/Logo_CSG-1920w-removebg-preview.png" alt="Logo Equipe 1" class="Logo1">
+                        <p class="mx-auto">VS</p>
+                        <img src="../assets/logo/Logo_CSG-1920w-removebg-preview.png" alt="Logo Equipe 2" class="Logo2">
+                    </div>
+                    <div class="bottom">
+                        <p>pré-national</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
     </div>
-    <div class="containerActu">
-        <div class="Actu">
-            <p class="titleActu">Fête du sport </p>
-            <p class="textActu">Venez nous rejoindre a la fete du sport de notre dame de gravenchon le dimanche 3 septembre</p>
+    <div class="container scoreContainer">
+        <div class="title">
+            <a href="">
+                <p>nos dernière <br>Actualités</p>
+            </a>
+    <div class="row mb-2">
+    <div class="col-md-5">
+      <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative shadow">
+        <div class="col p-4 d-flex flex-column position-static shadow">
+          <p class="mb-0 text-primary fs-3">Recrutement</p>
+          <div class="mb-1 text-muted fs-5">3 octobre 2023</div>
+          <p>Adrien Pochet</p>
+          <p class="mb-auto p-3 fs-6">Bon retour chez toi Adrien Pochet et bonne saison 💪🏼</p>
+          <a href="#" class="fs-7 continue">Lire plus...</a>
         </div>
-        <div class="Actu">
-            <p class="titleActu">Fête du sport </p>
-            <p class="textActu">Venez nous rejoindre a la fete du sport de notre dame de gravenchon le dimanche 3 septembre</p>
+        <div class="col-auto d-none d-lg-block">
+          <img class="bd-placeholder-img" width="200" height="300" src="../assets/logo/pochet-adrien__picture__2018-2019-3679-7184.png">
+
         </div>
-        <div class="Actu">
-            <p class="titleActu">Fête du sport </p>
-            <p class="textActu">Venez nous rejoindre a la fete du sport de notre dame de gravenchon le dimanche 3 septembre</p>
-        </div>
+      </div>
     </div>
-</div>
+    <div class="col-md-5">
+      <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative border border-primary-subtle ">
+        <div class="col p-4 d-flex flex-column position-static ">
+          <p class="mb-0 text-primary fs-3">Recrutement</p>
+          <div class="mb-1 text-muted fs-5">3 octobre 2023</div>
+          <p>Adrien Pochet</p>
+          <p class="mb-auto p-3 fs-6">Bon retour chez toi Adrien Pochet et bonne saison 💪🏼</p>
+          <a href="#" class="fs-7 continue">Lire plus...</a>
+        </div>
+        <div class="col-auto d-none d-lg-block">
+          <img class="bd-placeholder-img" width="200" height="300" src="../assets/logo/pochet-adrien__picture__2018-2019-3679-7184.png">
+
+        </div>
+      </div>
+    </div>
+  </div>
 
 
 
@@ -226,4 +250,4 @@
 
 
 
-<?php include "components/footer.php" ?>
+                <?php include "components/footer.php" ?>
