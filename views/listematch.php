3f21feda99593250c809369759cc9cf22516bab6
Allan DEHORS <allandhs@MacBook-Pro-de-Allan.local>
2023-11-07 16:57:50 +0100

    ModalSuppresion

--- views/listematch.php
+++ views/listematch.php
@@ -21,30 +21,54 @@
             </thead>
             <tbody>
                 <?php foreach (Matchs::getAllMatchs() as $match) : ?>
-                <tr>
-                    <th scope="row"><?= $match ['mat_id']?></th>
-                    <td><?= htmlspecialchars($match['mat_date']); ?></td>
-                    <td><?= htmlspecialchars(Equipe::getEquipesInfo($match['equ_id'])['equ_name']); ?></td>
-                    <td><?= htmlspecialchars(Equipe::getEquipesInfo($match['equ_id_equipes'])['equ_name']); ?></td>
-                    <td><?= htmlspecialchars($match['com_name']); ?></td>
-                    <td><?= htmlspecialchars($match['cat_name']); ?></td>
-                    <td><?= htmlspecialchars($match['mat_place']); ?></td>
-                    <td>
-                        <div class="d-grid gap-2 d-md-flex justify-content-md-end">
-                            <a href="#"><button class="btnModify">Modifier</button></a>
-                            <a href="#"><button class="btnDelete">Supprimer</button></a>
-                        </div>
-                    </td>
-                </tr>
+                    <tr>
+                        <th scope="row"><?= $match['mat_id'] ?></th>
+                        <td><?= htmlspecialchars($match['mat_date']); ?></td>
+                        <td><?= htmlspecialchars(Equipe::getEquipesInfo($match['equ_id'])['equ_name']); ?></td>
+                        <td><?= htmlspecialchars(Equipe::getEquipesInfo($match['equ_id_equipes'])['equ_name']); ?></td>
+                        <td><?= htmlspecialchars($match['com_name']); ?></td>
+                        <td><?= htmlspecialchars($match['cat_name']); ?></td>
+                        <td><?= htmlspecialchars($match['mat_place']); ?></td>
+                        <td>
+                            <div class="d-grid gap-2 d-md-flex justify-content-md-end">
+                                <a href="#"><button class="btnModify">Modifier</button></a>
+                                <!-- Button trigger modal -->
+                                <button type="button" class="btnDelete" data-bs-toggle="modal" data-bs-target="#modal<?=$match['mat_id']?>">
+                                    Supprimer
+                                </button>
+
+                                <!-- Modal -->
+                                <div class="modal fade" id="modal<?=$match['mat_id']?>" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                    <div class="modal-dialog">
+                                        <div class="modal-content">
+                                            <div class="modal-header">
+                                                <h1 class="modal-title fs-5" id="exampleModalLabel">Supprimer le match</h1>
+                                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                            </div>
+                                            <div class="modal-body">
+                                                Voulez-vous vraiment supprimer ce match ?
+                                            </div>
+                                            <div class="modal-footer">
+                                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                                <form action=""><button type="button" class="btn btn-primary">Supprimer</button></form>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </td>
+                    </tr>
                 <?php endforeach; ?>
             </tbody>
         </table>
     </div>
 </div>
+
+
 <div class="text-center mt-5">
-        <a href="../controllers/controller-ajoutmatch.php"><button class="btnModify">Ajouter un match</button></a>
-        <a href="../controllers/controller-paneladmin.php"><button class="btnDelete">Annuler</button></a>
-    </div>
+    <a href="../controllers/controller-ajoutmatch.php"><button class="btnModify">Ajouter un match</button></a>
+    <a href="../controllers/controller-paneladmin.php"><button class="btnDelete">Annuler</button></a>
+</div>
 
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
